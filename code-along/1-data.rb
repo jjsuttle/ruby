6cc6947a37b1863adb4ea79190ee24b483e2b9d0
jjsuttle <john.suttle@kellogg.northwestern.edu>
2022-04-22 19:07:51 +0000

    save

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -1,19 +1,32 @@
 # To run this code, be sure your current working directory
 # is the same as where this file is located and then run:
+
 # ruby 1-data.rb
+# puts "Hello, world" 
+# puts <- prints out the code 
 
 # In Ruby, there are different *types* of data:
 
 # Numbers
+# puts 7
 
 # Perform simple math with numbers
+# puts 7*8
 
 # Strings
+# puts "tacos"
 
-# Combine strings together
+# Combine strings together (the {} allows you to run code/calculations within a string)
+# puts "#{3+3+3} tacos"
 
 # Variables
+quantity = 3+3+3
+food = "tacos"
 
 # Combine strings and variables
+# puts "#{quantity} #{food}"
 
 # String manipulation
+# puts "hello".upcase
+# puts "hello".reverse
+# puts "hello".swapcase
